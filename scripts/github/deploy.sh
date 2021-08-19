07e4f4c945a5a4ff3e40cb3a2170a81d7547f784
Phil Benson <phil.benson@defra.gov.uk>
2021-08-19 16:54:15 +0100

    Publish to npm

--- scripts/github/deploy.sh
+++ scripts/github/deploy.sh
@@ -83,7 +83,7 @@ git push origin "${NEW_VERSION}"
 
 # Publish packages to npm
 echo "Publishing latest packages to npm"
-lerna publish --registry=http://registry.npmjs.org/ from-git --yes --pre-dist-tag rc
+npm publish
 
 # If we've pushed a new release into master and it is not a hotfix/patch, then merge the changes back to develop
 if [ "${BRANCH}" == "master" ] && [ "${RELEASE_TYPE}" != "patch" ]; then
